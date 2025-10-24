4c3212dad0b70b8d5620221b7851e9ec69117d74
Samuel Quintano <samuelmalagonero21@gmail.com>
2025-10-24 03:21:52 +0200

    pene

---
+++ panelContenido/panelContenido/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace panelContenido
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
